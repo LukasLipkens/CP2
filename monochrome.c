13d6ee50f6468f6f8cc40bc0a89e2d384bb2c06d
thibault luyts <Thibault.Luyts@student.pxl.be>
2022-05-03 10:07:29 +0200

    Create monochrome.c
    voorbeeld monochrome programma

---
+++ monochrome.c
@@ -0,0 +1,29 @@
+// zou misg werken
+
+Public Function ToBlackAndWhite(ByVal bmp As Bitmap) As Bitmap
+        Dim x As Integer
+        Dim y As Integer
+        Dim gem As Integer
+        Dim r, g, b As Integer
+        Dim col As Color
+
+        For x = 0 To bmp.Width - 1
+            For y = 0 To bmp.Height - 1
+
+                col = bmp.GetPixel(x, y)
+                r = col.R
+                g = col.G
+                b = col.B
+                gem = (r + g + b) / 3
+
+                If gem > 128 Then
+                    bmp.SetPixel(x, y, Color.White)
+                Else
+                    bmp.SetPixel(x, y, Color.Black)
+                End If
+
+            Next y
+        Next x
+
+        ToBlackAndWhite = bmp
+    End Function
